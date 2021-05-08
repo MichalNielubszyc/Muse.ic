eed1b51effa6cde5fe1f0c15b5fafe3ef4717100
Michal Nielubszyc <m.nielubszyc@gmail.com>
2021-05-08 16:24:52 +0200

    problems with serializing data returned from getStaticProps

--- muse-ic/pages/index.js
+++ muse-ic/pages/index.js
@@ -3,6 +3,16 @@ import { createGlobalStyle } from "styled-components";
 import { HeroSection } from "../components/1-hero-section/HeroSection";
 import { SignUpSection } from "../components/3-sing-up-section/SingUpSection";
 
+export const getStaticProps = async () => {
+  const response = await fetch("https://itunes.apple.com/search?term=arctic+monkeys&entity=album&limit=7") ?? null;
+  const data = response.json();
+
+  return {
+    props: { fetchedAlbums: data ?? null }
+  }
+  console.log(response)
+}
+
 const GlobalStyle = createGlobalStyle`
   body {
   padding: 0;
@@ -12,9 +22,10 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-export default function Home() {
+export default function Home({ fetchedAlbums }) {
   return (
     <>
+    {/* {console.log(fetchedAlbums)} */}
       <GlobalStyle />
       <Head>
         <title>Muse.ic</title>
